ee0b948b970571c281a59d8f493335e9509e5e1d
eileenmcg <33273648+eileenmcg@users.noreply.github.com>
2017-11-01 22:31:36 +0000

    Delete Task2a.py

--- Task2a.py
+++
@@ -1,9 +0,0 @@
-def reverse(input_string):
-    if len(input_string) == 0:
-        return input_string
-    else:
-        return reverse(input_string[1:]) + input_string[0]
-
-print ("Please enter the string you want to reverse: ")
-initial_input = input()
-print(reverse(initial_input))
